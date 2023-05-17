e4118c65a84b683c24df7e975251d2d16652eb01
jawadSaleem <jawadsaleem932@gmail.com>
2023-05-17 18:54:19 +0500

    Established database connection and created databaseConnection file

---
+++ src/main/java/com/mycompany/dbConnection/databaseConnection.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dbConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Jawad Saleem
+ */
+public class databaseConnection {   
+    static Connection conn; 
+    static String driver = "com.microdoft.sqlserver.jdbc.SQLServerDriver";
+    static String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=quiz_management_db;encrypt=true;trustServerCertificate=true";
+    static String userName = "jawad";
+    static String pass = "jawad";
+    public static Connection getConnection() throws SQLException{
+        if(conn == null){
+             //registering driver
+        try{
+        Class.forName(driver);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        //establishing connection through driver
+        conn = DriverManager.getConnection(url,userName,pass);
+        }
+       
+        return conn;
+    }
+}
